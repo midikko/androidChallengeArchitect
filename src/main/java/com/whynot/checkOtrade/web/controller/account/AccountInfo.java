270cafa19369d54a4d81b48be94c647781e9da05
addboynick <nevezhin.pavel@gmail.com>
2014-11-23 21:44:07 +0300

    личный кабинет

---
+++ src/main/java/com/whynot/checkOtrade/web/controller/account/AccountInfo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.whynot.checkOtrade.web.controller.account;
+
+/**
+ *
+ * @author masterkrab
+ */
+public class AccountInfo {
+
+	private String name;
+
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
